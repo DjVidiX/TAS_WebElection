4b9a17dac0fb105b8669c2986382720385f2f519
DjVidiX <djvidix.ms@gmail.com>
2013-11-27 15:59:17 +0100

    testowanie IDE

--- TAS_WebElection/views.py
+++ TAS_WebElection/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render_to_response
 
-def home(request):
-	return render_to_response('main.html')
 
+def home(request):
+    return render_to_response('main.html')
+
+
+def candidates(request):
+    lista = ['Marek', 'Lukasz']
+    return render_to_response('candidates.html', {'listahtml': lista})
+
+
+def vote(request):
+    return render_to_response('vote.html')
+
+
+def index(request):
+    return render_to_response('index.html')
