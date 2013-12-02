0d30b0ceb314f90f8383b10671d5120421f3c188
Łukasz Kapuściński <lukkap92@lukkap92-ntbk.(none)>
2013-12-02 22:39:24 +0100

    koniec poniedzialku

---
+++ voting/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from voting.models import Kandydat
+
+admin.site.register(Kandydat)
--- voting/models.py
+++ voting/models.py
@@ -1,1 +1,10 @@
+from django.db import models
+
+class Kandydat(models.Model):
+	imie = models.CharField(max_length=30)
+	nazwisko = models.CharField(max_length=30)
+	zdjecie = models.ImageField(upload_to='static/candidates')
+
+	
+
 # Create your models here.
