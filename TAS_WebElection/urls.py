6825a179492709b165ac85f2e2238e6a248d324d
Łukasz Kapuściński <lk67718@st.amu.edu.pl>
2013-12-03 17:02:45 +0100

    zmienione viewsy i pelna sciezka

--- TAS_WebElection/urls.py
+++ TAS_WebElection/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from TAS_WebElection.views import home, candidates, vote, index
+from TAS_WebElection.views import home, candidates, contact
 
 
 # Uncomment the next two lines to enable the admin:
@@ -11,14 +11,12 @@ urlpatterns = patterns('',
                        # Examples:
                        url(r'^$', home),
                        url(r'^kandydaci', candidates, name='candidates'),
-                       url(r'^glosuj', vote, name='vote'),
-                       url(r'^lol', index, name='lool'),
-                       url(r'^lol#admin', candidates, name='candidates'),
+                       url(r'^kontakt', contact, name='contact'),
                        # url(r'^TAS_WebElection/', include('TAS_WebElection.foo.urls')),
 
                        # Uncomment the admin/doc line below to enable admin documentation:
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
                        # Uncomment the next line to enable the admin:
-                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^administrator/', include(admin.site.urls)),,
 )
--- TAS_WebElection/views.py
+++ TAS_WebElection/views.py
@@ -15,10 +15,5 @@ def candidates(request):
     lista = ['Marek', 'Lukasz']
     return render_to_response('candidates.html', {'listahtml': lista})
 
-
-def vote(request):
-    return render_to_response('vote.html')
-
-
-def index(request):
-    return render_to_response('index.html')
+def contact(request):
+    return render_to_response('contact.html')
