f797f742233dbc04cd40c9365a0cfcc264383f67
DjVidiX <djvidix.ms@gmail.com>
2013-12-04 18:24:32 +0100

    Update forms.py

--- TAS_WebElection/forms.py
+++ TAS_WebElection/forms.py
@@ -6,4 +6,4 @@ from django import forms
 class ContactForm(forms.Form):
     tytul = forms.CharField()
     email = forms.EmailField(required=False)
-    wiadomosc = forms.CharField()
+    wiadomosc = forms.CharField(widget=forms.Textarea)
