ee3e52962cdae55a7d7883433a33050a2331a368
DjVidiX <djvidix.ms@gmail.com>
2013-12-10 12:58:33 +0100

    sending mail fix

--- TAS_WebElection/forms.py
+++ TAS_WebElection/forms.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 __author__ = 'marek'
 
 from django import forms
@@ -7,3 +9,11 @@ class ContactForm(forms.Form):
     tytul = forms.CharField()
     email = forms.EmailField(required=False)
     wiadomosc = forms.CharField(widget=forms.Textarea)
+
+    def clean_message(self):
+        message = self.cleaned_data['wiadomosc']
+        num_words = len(message.split())
+        if num_words < 4:
+            raise forms.ValidationError(u"Za mało słów")
+        return message
+
