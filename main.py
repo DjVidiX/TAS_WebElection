68eb441a80bf24b4768541bf9e2507b1f0222a55
Marek Stereńczak <marek@MaraS-Toshiba.home>
2014-01-26 23:38:18 +0100

    Boot added

---
+++ main.py
@@ -0,0 +1,85 @@
+__author__ = 'marek'
+
+import string, random, sqlite3, pycurl
+
+
+def randomStringGenerator(size):
+    return ''.join(random.choice(string.ascii_letters) for x in range(size))
+
+
+def randomNumberGeneraor(size):
+    return ''.join(random.choice(string.digits) for x in range(size))
+
+
+def randomNumberStringGenerator(size):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for x in range(size))
+
+
+
+def generateUsers():
+    file = open("users.txt", "w")
+
+    for i in range(100):
+        user_list = []
+        user_list.append(i.__str__())
+        name = randomStringGenerator(6)
+        user_list.append(name)
+        lastName = randomStringGenerator(8)
+        user_list.append(lastName)
+        pesel = randomNumberGeneraor(11)
+        user_list.append(pesel)
+        nrDowodu = randomNumberStringGenerator(9)
+        user_list.append(nrDowodu)
+        glosowal = False
+        user_list.append(glosowal.__str__())
+        user_list.append("\n")
+        file.write(", ".join(user_list))
+
+
+    file.close()
+
+
+def main():
+    try:
+        file = open("users.txt", 'r')
+    except IOError:
+        generateUsers()
+        file = open("users.txt", 'r')
+        conn = sqlite3.connect('database.db')
+
+        c = conn.cursor()
+        lines = []
+        for line in file.readlines():
+            values = line.split(",")
+            values.pop()
+            print values
+            c.execute("INSERT INTO voting_obywatel VALUES (?, ?, ?, ?, ?, ?)", values)
+            lines.append(values)
+
+        conn.commit()
+
+        # file = open("users.txt", 'wba')
+        # generateUsers(file)
+    lines = []
+    for line in file.readline():
+        values = line.split(",")
+        values.pop()
+        lines.append(values)
+
+    c = pycurl.Curl()
+    c.setopt(c.URL, "localhost:8000")
+
+    # for line in lines:
+    cmd = 'firstName=%s&lastName=%s&pesel=%s&password=%s' % (lines[1], lines[2], lines[3], lines[4])
+    # c.setopt(c.POSTFIELDS, cmd)
+    c.setopt(c.COOKIEFILE, '')
+    c.setopt(c.VERBOSE, False)
+
+    c.perform()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
